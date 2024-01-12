d87968da4f0a8fd188b38dcc143e015a28a62625
Tyler <psenk168@gmail.com>
2024-01-12 00:13:39 -0600

    changed to foki bot, connected to sith server with bingowhen command. google docs connection completed. submissions tracked with uuids.

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import uuid
 
 ### variables and constants
 num_submissions = 0
-
 load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
@@ -17,18 +16,18 @@ client = discord.Client(intents=intents)
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 GOOGLE_DOCS_KEY = os.getenv("GOOGLE_DOCS_KEY")
 handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
-SUBMISSIONS_CHANNEL = 1194832130962890842
-LOGS_CHANNEL = 1194488938480537740
-WEBHOOK_USER_ID = 1194889597738549298
 
+TEST_SUBMISSIONS_CHANNEL = 1194832130962890842
+TEST_LOGS_CHANNEL = 1194488938480537740
+TEST_WEBHOOK_USER_ID = 1194889597738549298
+
+BINGO_GENERAL_CHANNEL = 1193039460980502578
 
 map_of_submissions = {}
 map_of_embeds = {}
 map_of_embed_messages = {}
 
-
-# submission responses
-submission_responses_dict = {
+foki_responses_dict = {
     1: "DID YOU REMEMBER YOUR CODEWORD?",
     2: "REALLY? THIS IS YOUR SUBMISSION? ARE YOU SURE?",
     3: "YOU'RE THE FIRST PERSON TO SUBMIT THIS! PROBABLY. MAYBE.",
@@ -60,12 +59,17 @@ async def connect_to_google():
     )
     service = build("docs", "v1", credentials=credentials)
 
-    #print("{0.user}: Connected to Google API.".format(client))
+    print(
+        "{0.user}: Captain, we've started a communications link between us and Starfleets Google API division.".format(
+            client
+        )
+    )
 
     # location = service.documents().get(documentId=GOOGLE_DOCS_KEY).body.content.endIndex
     # print(location)
 
     return service
+
 
 # function to post image to doc
 async def post_image(image_url, id):
@@ -73,18 +77,26 @@ async def post_image(image_url, id):
     service = await connect_to_google()
 
     # adding image to document
-    request_one = [{"insertInlineImage": {"location": {"index": 1}, "uri": image_url}}]
-    request_two = [{"insertText": {"location": {"index": 1}, "text": "{0}\n".format(id)}}]
-    request_three=[{"insertPageBreak": {"location": {"index": 1}}}]
-    
-    response_one = (
-        service.documents().batchUpdate(documentId=GOOGLE_DOCS_KEY, body={"requests": request_three}).execute()
-    )    
-    response_two = (
-        service.documents().batchUpdate(documentId=GOOGLE_DOCS_KEY, body={"requests": request_one}).execute()
+    insert_image = [{"insertInlineImage": {"location": {"index": 1}, "uri": image_url}}]
+    insert_text = [
+        {"insertText": {"location": {"index": 1}, "text": "{0}\n".format(id)}}
+    ]
+    insert_page_break = [{"insertPageBreak": {"location": {"index": 1}}}]
+
+    update_1 = (
+        service.documents()
+        .batchUpdate(documentId=GOOGLE_DOCS_KEY, body={"requests": insert_page_break})
+        .execute()
     )
-    response_three = (
-        service.documents().batchUpdate(documentId=GOOGLE_DOCS_KEY, body={"requests": request_two}).execute()
+    update_2 = (
+        service.documents()
+        .batchUpdate(documentId=GOOGLE_DOCS_KEY, body={"requests": insert_text})
+        .execute()
+    )
+    update_3 = (
+        service.documents()
+        .batchUpdate(documentId=GOOGLE_DOCS_KEY, body={"requests": insert_image})
+        .execute()
     )
     # insert_inline_image_response = response.get("replies")[0].get("insertInlineImage")
 
@@ -94,114 +106,145 @@ async def post_image(image_url, id):
             client
         )
     )
-    print("[] Received: Submission # {0}".format(id))
+    trimmedId = id[:8]
+    print("[] Received: Submission # {0}".format(trimmedId))
+
 
 # posts custom embed to logs channel
 async def submission_alert(message, id):
     global num_submissions, map_of_embed_messages, map_of_embeds
+    trimmedId = id[:8]
     d = datetime.datetime.now()
-    channel = client.get_channel(LOGS_CHANNEL)
+    channel = client.get_channel(TEST_LOGS_CHANNEL)
     embed = discord.Embed(
-        title=f"Submission #\n{id}", url=message.jump_url, color=0x0A0AAB
+        title=f"Submission #\n{trimmedId}", url=message.jump_url, color=0x0A0AAB
     )
     embed.set_author(name=message.author, icon_url=message.author.avatar)
     embed.set_thumbnail(url=message.attachments[0].url)
-    embed.add_field(name="Posted on: ", value=d.strftime("%B %d %X"))
-    
-    await channel.send(embed=embed)
-    map_of_embed_messages[num_submissions] = message
-    map_of_embeds[num_submissions] = embed
+    embed.add_field(name="Posted on: /n", value=d.strftime("%B %d %X"))
 
-    
+    embed_message = await channel.send(embed=embed)
+    map_of_embed_messages[id] = embed_message
+    map_of_embeds[id] = embed
+
+
+# approve
 async def approve(message):
     global map_of_submissions
     id = message.content.split()[5]
-    
+    trimmedId = id[:8]
+
     d = datetime.datetime.now()
-    channel = client.get_channel(LOGS_CHANNEL)    
-    emoji = '✅'   
-    
-    print("Message ID: {0}".format(id))
-    print(map_of_submissions.keys)
-    print("Message: {0}".format(map_of_submissions.get(id)))
-    
+    channel = client.get_channel(TEST_LOGS_CHANNEL)
+    emoji = "✅"
+
     await map_of_submissions.get(id).add_reaction(emoji)
-    print("{0.user}: Captain, a submission has been approved.".format(client))
-        
+    print(
+        "{0.user}: Captain, submission {1} has been approved.".format(client, trimmedId)
+    )
+
     embed = map_of_embeds.get(id)
     embed_message = map_of_embed_messages.get(id)
-    embed.add_field(name="Approved on: ", value=d.strftime("%B %d %X"))
-    
+    embed.add_field(name="Approved on: /n", value=d.strftime("%B %d %X"))
+
     await embed_message.delete()
     await channel.send(embed=embed)
     return
 
+
 ### discord code
 
 
-# login event, triggers once bot is ready and working
 @client.event
+# login event, triggers once bot is ready and working
 async def on_ready():
     # log
     print("{0.user}: Captain, the warp core is ready to go.".format(client))
 
 
-# message event, triggers when a message is sent
 @client.event
+# message event, triggers when a message is sent
 async def on_message(message):
     global num_submissions, map_of_submissions
+
+    # ignores own messages
     if message.author == "client.user":
         return
-    
+
+    # responses from the google api
     if message.content.startswith("Google"):
         await approve(message)
-        
-    # targeting a specific channel
-    if message.channel == client.get_channel(SUBMISSIONS_CHANNEL):
-        ### bingosubmit command
-        if message.content.startswith("!bingosubmit"):
-            await message.channel.send("`Processing...`")
-            if message.attachments:
-                for attachment in message.attachments:
-                    if attachment.filename.endswith((".png", ".jpg", "jpeg", "gif")):
-                        num_submissions += 1
-                        id = uuid.uuid4();
-                        map_of_submissions[id] = message
-                        
-                        # posting to google sheets
-                        await post_image(attachment.url, id)
+        return
 
-                        # posting to #posthere channel
-                        await message.channel.send(
-                            "`Your submission has been successfully submitted to the Bingo council.`"
-                        )
-                        
-                        # snarky options if desired
-                        #bot_response = random.randint(1, 10)
-                        #await message.channel.send(submission_responses_dict[bot_response])
+    # targeting messages from Sith #bingo-general
+    if message.channel == client.get_channel(BINGO_GENERAL_CHANNEL):
+        if message.content.startswith("!bingo"):
+            cmd = message.content[6:]
 
-                        # posting logs to #logs channel
-                        await submission_alert(message, id)
-                        return
-                    
-                    # wrong file type
-                    else:
-                        await message.channel.send("`The file type you have submitted is not supported.  Please use .png, .jpg, .jpeg, or .gif.`")
-                        return
+            ### bingowhen  command
+            if cmd == "when":
+                await message.channel.send("👀")
 
-            # no attachment on message
-            else:
-                await message.channel.send("`There was no attachment on that post. Your submission has been rejected.`")
-                return
+    # targeting the test submissions channel
+    if message.channel == client.get_channel(TEST_SUBMISSIONS_CHANNEL):
+        if message.content.startswith("!bingo"):
+            cmd = message.content[6:]
+            await message.channel.send("Processing...")
+
+            ### bingosubmit command
+            if cmd == "submit":
+                if message.attachments:
+                    for attachment in message.attachments:
+                        if attachment.filename.endswith(
+                            (".png", ".jpg", "jpeg", "gif")
+                        ):
+                            num_submissions += 1
+                            id = str(uuid.uuid4())
+
+                            map_of_submissions[id] = message
+
+                            # posting to google sheets
+                            await post_image(attachment.url, id)
+
+                            # posting to #posthere channel
+                            await message.channel.send(
+                                "Your submission has been successfully submitted to the Bingo council."
+                            )
+
+                            # snarky options if desired
+                            # bot_response = random.randint(1, 10)
+                            # await message.channel.send(submission_responses_dict[bot_response])
+
+                            # posting logs to #logs channel
+                            await submission_alert(message, id)
+                            return
+
+                        # wrong file type
+                        else:
+                            await message.channel.send(
+                                "The file type you have submitted is not supported.  Please use .png, .jpg, .jpeg, or .gif."
+                            )
+                            return
+
+                # no attachment on message
+                else:
+                    await message.channel.send(
+                        "There was no attachment on that post. Your submission has been rejected."
+                    )
+                    return
+
+        # quick response action
         elif message.content.startswith("!ping"):
             await message.channel.send("pong")
             return
+
+
 # runs the bot
 client.run(DISCORD_TOKEN, log_handler=handler, log_level=logging.DEBUG)
 
+
 # IDEAS
-# - submit command
-# - - send to google sheets?
+
 # - rules command
 # - tiles/map command
 # - teams command
